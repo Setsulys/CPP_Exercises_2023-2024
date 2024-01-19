362f86bbcac5c98a15ee4ad24fdc2d5a56869979
LY-IENG Steven <steven.ly412@gmail.com>
2024-01-19 11:53:58 +0100

    tp1 -ex2 q7

--- tp1/ex2/main.cpp
+++ tp1/ex2/main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+#include <vector>
 #include "Person.hpp"
+
+using namespace std;
 int main(int argc, char const *argv[])
 {
-    auto p = Person("Paluche","LaFaluche");
-    std::cout << "La personne s'appelle "<<p.get_name() <<" "<<p.get_surname() << std::endl;
+    vector<Person> persons;
+    int nbPersonnes;
+    cout << "Nombre de personnes : ";
+    cin >> nbPersonnes;
+    string name;
+    string surname;
+    for(auto i =0; i  < nbPersonnes; i++)
+    {
+        cout << "Prenom: ";
+        cin >> name;
+        cout << "Nom: ";
+        cin >> surname;
+        persons.emplace_back(Person(name,surname));
+    }
+    cout << "Les personnes sont";
+    for(auto p : persons)
+    {
+        cout <<" "<< p.get_name()<<" " << p.get_surname();
+    }
+    cout <<"." << endl;
     return 0;
 }
