69abfc1ab5b93dc0e40527ac4166f2517863806b
LY-IENG Steven <steven.ly412@gmail.com>
2024-03-08 08:32:43 +0100

    remove last tp

--- tp4/ex2/tests/07-multi-inclusions.cpp
+++
@@ -1,9 +0,0 @@
-#include "../src/PhoneNumber.hpp"
-
-// Force une double-inclusion
-#include "../src/PhoneNumber.hpp"
-
-#include <catch2/catch_test_macros.hpp>
-
-TEST_CASE("Le fichier PhoneNumber.hpp est protégé contre les inclusions multiples")
-{}
